a77c0d3682d0d6956e425c74042c17944cd80d7f
Keemyoonho <kimyoonho0920@gmail.com>
2026-08-16 22:33:48 +0900

    Remove duplicate files

--- theme.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    const savedTheme = localStorage.getItem('yoonho_theme');
-    const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    document.documentElement.dataset.theme = savedTheme || (prefersDark ? 'dark' : 'light');
-  })();
